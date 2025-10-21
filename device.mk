d8d9d38d54a56532eb4306a32102802e007786eb
Faris <rsuntk@yukiprjkt.my.id>
2025-10-21 11:22:41 +0700

    X00TD: Commonize biometrics
    Signed-off-by: Faris <rsuntk@yukiprjkt.my.id>

--- device.mk
+++ device.mk
@@ -11,10 +11,6 @@ $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service-X00TD
-
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
