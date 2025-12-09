ff55418c7b699e456062815e71fcd37291090e78
Kenaidi01 <andrianp920@gmail.com>
2025-12-09 09:55:52 +0000

    X00TD: include my private-key

--- device.mk
+++ device.mk
@@ -7,6 +7,8 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/asus/sdm660-common/sdm660.mk)
 
+-include vendor/evolution-priv/keys/keys.mk
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2160
 TARGET_SCREEN_WIDTH := 1080
